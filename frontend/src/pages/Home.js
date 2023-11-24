94e6847d363c6d2695617762cc7a9a86ea193a62
KachikwuNwike <kachi.nwike@yahoo.com>
2023-11-24 12:11:00 -0500

    Implemented the autocomplete feature

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,7 +9,6 @@ export default function Home() {
     return (
         <div className="App">
         <Header />
-        {/* <Autocomplete/> */}
         <SearchBar />
         <TrendingKeywords />
         <Footer />
